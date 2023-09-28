71e3bd56f857852f21529e820828fa086119f419
Ismail Ahmedov <ismail.ahmedov@asesormasmovil.es>
2023-09-28 03:44:54 +0200

    Add instance method to Record

--- src/test/java/com/github/ismail2ov/CarRecord.java
+++ src/test/java/com/github/ismail2ov/CarRecord.java
@@ -1,3 +1,9 @@
 package com.github.ismail2ov;
 
-public record CarRecord(String regNumber, String owner) {}
+public record CarRecord(String regNumber, String owner) {
+
+    public boolean isNewCar() {
+        return regNumber().substring(3, 3).equals("MDG");
+    }
+
+}
--- src/test/java/com/github/ismail2ov/RecordsTest.java
+++ src/test/java/com/github/ismail2ov/RecordsTest.java
@@ -20,5 +20,17 @@ class RecordsTest {
         assertThat(carRecord.toString()).isNotEqualTo(car.toString());
         assertThat(carRecord.regNumber()).isEqualTo(car.getRegNumber());
         assertThat(carRecord.owner()).isEqualTo(car.getOwner());
+
+        assertThat(carRecord.isNewCar()).isFalse();
+    }
+
+    @Test
+    void testRecordWithInstanceMethod() {
+        String regNumber = "4794YBK";
+        String name = "John Doe";
+
+        CarRecord carRecord = new CarRecord(regNumber, name);
+
+        assertThat(carRecord.isNewCar()).isFalse();
     }
 }
