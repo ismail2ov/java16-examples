41077444d46315504e777dfc50d9dd1e03b56e8a
Ismail Ahmedov <ismail.ahmedov@asesormasmovil.es>
2023-09-28 03:49:39 +0200

    Add static field to Record

--- src/test/java/com/github/ismail2ov/CarRecord.java
+++ src/test/java/com/github/ismail2ov/CarRecord.java
@@ -2,8 +2,10 @@ package com.github.ismail2ov;
 
 public record CarRecord(String regNumber, String owner) {
 
+    public static final String LICENSE_PLATE_LETTERS = "MDG";
+
     public boolean isNewCar() {
-        return regNumber().substring(3, 3).equals("MDG");
+        return regNumber().substring(3, 3).equals(LICENSE_PLATE_LETTERS);
     }
 
 }
